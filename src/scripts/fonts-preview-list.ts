91661bece1122d069ba9d8295dbc05ff78fad4ed
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2025-10-10 14:50:06 +0200

    add font categories filter

--- src/scripts/fonts-preview-list.ts
+++ src/scripts/fonts-preview-list.ts
@@ -23,3 +23,45 @@ fontPreviewItalicInput.addEventListener("change", (event) => {
     isItalic ? "italic" : "normal"
   );
 });
+
+// font categories input
+const fontPreviewCategoriesDiv = document.getElementById(
+  "font-preview-categories"
+);
+if (fontPreviewCategoriesDiv) {
+  fontPreviewCategoriesDiv.addEventListener("change", () => {
+    // Get all checked checkboxes inside the categories div
+    const checkedBoxes = fontPreviewCategoriesDiv.querySelectorAll(
+      'input[type="checkbox"]:checked'
+    );
+    const selectedCategories = Array.from(checkedBoxes)
+      .map((checkbox) => {
+        if (checkbox instanceof HTMLInputElement) {
+          return checkbox.value;
+        }
+        return "";
+      })
+      .filter(Boolean);
+
+    // if NO checkbox is selected THEN all are displayed
+    const detailsList = Array.from(
+      document.querySelectorAll("details[font-categories]")
+    );
+    if (selectedCategories.length === 0) {
+      detailsList.forEach((detail) => {
+        detail.removeAttribute("hidden");
+      });
+      return;
+    }
+
+    // if some are selected then hidden all EXCEPT the ones with corresponding category
+    detailsList.forEach((detail) => {
+      const attr = detail.getAttribute("font-categories");
+      const fontCategories = attr ? attr.split(", ") : [];
+      const isVisible = selectedCategories.some((category) =>
+        fontCategories.includes(category)
+      );
+      (detail as HTMLElement).hidden = !isVisible;
+    });
+  });
+}
