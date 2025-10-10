fa2d10dd16b46dc018a6815176d2a92427ad098b
Raphael Ferrand <goldenraphti@users.noreply.github.com>
2025-10-10 14:39:40 +0200

    add forms inputs to preview size + weight + italic

---
+++ src/scripts/fonts-preview-list.ts
@@ -0,0 +1,25 @@
+const fontsPreviewList = document.getElementById("fonts-preview-list");
+
+// font-size input
+const fontPreviewSizeInput = document.getElementById("font-preview-size");
+fontPreviewSizeInput.addEventListener("input", (event) => {
+  const fontSize = event.target.value;
+  fontsPreviewList.style.setProperty("--font-size-preview", `${fontSize}rem`);
+});
+
+// font-weight input
+const fontPreviewWeightInput = document.getElementById("font-preview-weight");
+fontPreviewWeightInput.addEventListener("input", (event) => {
+  const fontWeight = event.target.value;
+  fontsPreviewList.style.setProperty("--font-weight-preview", fontWeight);
+});
+
+// font italic input
+const fontPreviewItalicInput = document.getElementById("font-preview-italic");
+fontPreviewItalicInput.addEventListener("change", (event) => {
+  const isItalic = event.target.checked;
+  fontsPreviewList.style.setProperty(
+    "--font-italic-preview",
+    isItalic ? "italic" : "normal"
+  );
+});
